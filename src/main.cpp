77fad34dfa466527ac81d7c90b55f3060a22a31e
sei-kiu <sei.kiu.sei.kiu@gmail.com>
2022-02-02 20:37:12 +0800

    working code. FORWARD, REVERSE, LEFT, RIGHT, STOP

---
+++ src/main.cpp
@@ -0,0 +1,123 @@
+#include <Arduino.h>
+#include <MeAuriga.h>
+
+#define FORWARD 1
+#define REVERSE 2
+#define LEFT 3
+#define RIGHT 4
+#define STOP 5
+
+MeEncoderOnBoard Encoder_1(SLOT1);
+MeEncoderOnBoard Encoder_2(SLOT2);
+
+void isr_process_encoder1(void);
+void isr_process_encoder2(void);
+
+void moveDuration(float seconds);
+void move(int direction, int speed);
+
+void setup()
+{
+  // Set PWM 8 KHz
+  TCCR1A = _BV(WGM10);
+  TCCR1B = _BV(CS11) | _BV(WGM12);
+  TCCR2A = _BV(WGM21) | _BV(WGM20);
+  TCCR2B = _BV(CS21);
+
+  attachInterrupt(Encoder_1.getIntNum(), isr_process_encoder1, RISING);
+  attachInterrupt(Encoder_2.getIntNum(), isr_process_encoder2, RISING);
+}
+
+void loop()
+{
+  // FORWARD at 25% for 2 second
+  move(FORWARD, 25 / 100.0 * 255);
+  moveDuration(2);
+
+  // REVERSE at 25% for 2 second
+  move(REVERSE, 25 / 100.0 * 255);
+  moveDuration(2);
+
+  // LEFT at 25% for 2 second
+  move(LEFT, 25 / 100.0 * 255);
+  moveDuration(2);
+
+  // RIGHT at 25% for 2 second
+  move(RIGHT, 25 / 100.0 * 255);
+  moveDuration(2);
+
+  // Stop moving
+  move(STOP, 0);
+  moveDuration(2);
+}
+
+void isr_process_encoder1(void)
+{
+  if (digitalRead(Encoder_1.getPortB()) == 0)
+  {
+    Encoder_1.pulsePosMinus();
+  }
+  else
+  {
+    Encoder_1.pulsePosPlus();
+  }
+}
+
+void isr_process_encoder2(void)
+{
+  if (digitalRead(Encoder_2.getPortB()) == 0)
+  {
+    Encoder_2.pulsePosMinus();
+  }
+  else
+  {
+    Encoder_2.pulsePosPlus();
+  }
+}
+
+void move(int direction, int speed)
+{
+  int leftSpeed = 0;
+  int rightSpeed = 0;
+  if (direction == FORWARD)
+  {
+    leftSpeed = -speed;
+    rightSpeed = speed;
+  }
+  else if (direction == REVERSE)
+  {
+    leftSpeed = speed;
+    rightSpeed = -speed;
+  }
+  else if (direction == LEFT)
+  {
+    leftSpeed = -speed;
+    rightSpeed = -speed;
+  }
+  else if (direction == RIGHT)
+  {
+    leftSpeed = speed;
+    rightSpeed = speed;
+  }
+  else if (direction == STOP)
+  {
+    leftSpeed = 0;
+    rightSpeed = 0;
+  }
+  Encoder_1.setTarPWM(leftSpeed);
+  Encoder_2.setTarPWM(rightSpeed);
+}
+
+void moveDuration(float seconds)
+{
+  if (seconds < 0.0)
+  {
+    seconds = 0.0;
+  }
+  unsigned long endTime = millis() + seconds * 1000;
+  while (millis() < endTime)
+  {
+    Encoder_1.loop();
+    Encoder_2.loop();
+  }
+}
